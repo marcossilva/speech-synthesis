eb0835f186d64f3257324d467d85c2eae796db90
Marcos Leal <marcossilva_604@yahoo.com.br>
2018-08-25 16:53:50 -0300

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project gather all the relevant data, information and analysis that I could find on the portuguese language to pursue my master's thesis.',
+    author='Marcos Leal',
+    license='MIT',
+)
